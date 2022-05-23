cf457887a14334595920f325fba86c882e3537e4
JuanSBdev <juansb.dev@gmail.com>
2022-05-23 00:49:26 -0300

    api

--- primer-app/src/App.js
+++ primer-app/src/App.js
@@ -1,19 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import imageRickAndMorty from './img/rick-morty.png';
+import {useState} from 'react';
+import Characters from './components/Characters'
 function App() {
+  const [characters, setCharacters] = useState(null);
 
   const reqApi = async () =>{
     const api = await fetch('https://rickandmortyapi.com/api/character');
-    const characterApi = await api.json()
-    console.log(characterApi);
+    const characterApi = await api.json();
+      setCharacters(characterApi.results);
   }
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Primer App</h1>
-        <img src={imageRickAndMorty} alt="Richie" className="img-home"></img>
-        <button onClick={reqApi} className='btn-search'>Buscar Personajes</button>
+        {characters ? (
+          <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Characters characters={characters}/>
+          </>
+        ) : (
+          <>
+          <img src={imageRickAndMorty} alt="Richie" className="img-home"></img>
+          <button onClick={reqApi} className='btn-search'>Buscar Personajes</button>
+          </>
+        )}
       </header>
     </div>
   );
